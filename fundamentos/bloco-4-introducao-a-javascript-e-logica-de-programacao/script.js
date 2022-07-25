e3f5fc31fe236c3091ca03ef495d5ee3f4afda48
Fabio Henrique Luiz <fabiohenrique@hotmail.com>
2022-07-25 19:51:23 -0300

    primeiros exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,44 @@
+// let soma1 = 10;
+// let soma2 = 15;
+
+// let soma = soma1 + soma2;
+// console.log(soma);
+
+// let num1 = 33;
+// let num2 = 15;
+
+// let sub = num1 - num2;
+// console.log(sub);
+
+// let num1 = 5;
+// let num2 = 8;
+
+// let mult = num1 * num2;
+// console.log(mult);
+
+// let num1 = 81;
+// let num2 = 9;
+
+// let div = num1 / num2;
+// console.log(div);
+
+// let num1 = 63;
+// let num2 = 117;
+
+// let mod = ((num1 + num2) %2) === 0;
+// console.log(mod);
+
+// let num1 = 10;
+// let num2 = 20;
+
+// let maior = num1 > num2;
+// console.log(maior);
+
+// const a = 20;
+// const b = 21;
+
+// if( a > b ){
+//   console.log(" a é maior que b ");
+// } else {
+//   console.log(" b é maior que a ");
+// };
