a329066837b48110300ada0876e2877e61c6302c
Fabio Henrique Luiz <fabiohenrique@hotmail.com>
2022-07-27 18:31:43 -0300

    Exercicios de Seletores de elementos

---
+++ fundamentos/bloco-5-JavaScript-DOM-Eventos-e-Web-Storage/script.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Interestelar";
+document.getElementById("second-paragraph").innerText = "Melhor filme de Ficção!";
+document.getElementById("subtitle").innerHTML = "Nos cinemas";
