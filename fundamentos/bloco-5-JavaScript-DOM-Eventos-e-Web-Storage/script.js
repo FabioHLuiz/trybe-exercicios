8c30857aa49d00d7e509a331d9e2a518f916ea27
Fabio Henrique Luiz <fabiohenrique@hotmail.com>
2022-07-28 15:48:39 -0300

    Exercicios de Seletores de elementos

--- fundamentos/bloco-5-JavaScript-DOM-Eventos-e-Web-Storage/script.js
+++ fundamentos/bloco-5-JavaScript-DOM-Eventos-e-Web-Storage/script.js
@@ -1,3 +1,13 @@
 document.getElementById("page-title").innerText = "Interestelar";
+
 document.getElementById("second-paragraph").innerText = "Melhor filme de Ficção!";
-document.getElementById("subtitle").innerHTML = "Nos cinemas";
+
+
+
+document.getElementsByClassName("para")[0].style.fontSize = "50px";
+
+document.getElementsByClassName("para")[1].style.fontSize = "100px";
+
+document.getElementsByTagName("h4")[0].innerText = "No UCI Cinemas";
+  
+document.getElementsByTagName("h4")[0].style.color = "red";
