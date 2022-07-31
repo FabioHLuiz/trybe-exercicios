106e33cf2703eb8406f8ab892da064c0f784305a
Fabio Henrique Luiz <fabiohenrique@hotmail.com>
2022-07-31 11:47:12 -0300

    Exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,102 @@
+// exercicio 1
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(numbers[i]);
+// }
+
+// exercicio 2
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   soma += numbers[i];
+// }
+// console.log(soma);
+
+// exercicio 3
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   soma += numbers[i];
+// }
+// let avg = soma / numbers.length;
+// console.log(avg);
+
+// exercico 4
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   soma += numbers[i];
+// }
+// avg = soma / numbers.length;
+
+// if (avg > 20) {
+//   console.log("Valor maior que 20")
+// } else {
+//   console.log("Valor menor ou igual a 20");
+// }
+
+// exercicio 5
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maior = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] > maior) {
+//     maior = numbers[i];
+//   }
+// }
+// console.log(maior);
+
+// exercicio 6
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let impar = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] % 2 !== 0) {
+//     impar += 1;
+//   }
+
+// }
+// if (impar === 0) {
+//   console.log("nenhum valor ímpar encontrado");
+// } else {
+//   console.log(impar);
+// }
+
+// exercicio 7
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let small = numbers[0];
+
+// for (let i = 1; i < numbers.length; i += 1) {
+//   if (numbers[i] < small) {
+//     small = numbers[i];
+//   }
+// }
+// console.log(small);
+
+// exercicio 8
+
+// const array = []
+// const size = 25
+// for (let i = 1; i <= size; i += 1) {
+//   array.push(i)
+// }
+// console.log(array);
+
+// exercicios 9
+
+// let array = []
+// let size = 25
+// for (let i = 1; i <= size; i += 1) {
+//   array.push(i)
+// }
+// for (let i = 0; i < array.length; i += 1) {
+//   console.log(array[i] / 2);
+// }
