f7f22452194b0f3c73c0aa7149b59c8849c380c3
Fabio Henrique Luiz <fabiohenrique@hotmail.com>
2022-08-15 16:10:12 -0300

    Exercicios de Conteudo B7 D2, Att

---
+++ fundamentos/bloco-7-Introdução-à-JavaScript-ES6-e-Testes-Unitários/dia-2-JavaScript-ES6-Fluxo-de-exceção-e-Objetos/exercicios-de-conteudo/Teste.js
@@ -0,0 +1,22 @@
+const animal = {
+  nome: 'Jade',
+  idade: 10,
+  comida: 'petisco',
+};
+// console.log(Object.keys(animal));
+
+const animal = {
+  nome: 'Jade',
+  idade: 10,
+  comida: 'petisco',
+};
+// console.log(Object.values(animal));
+
+const animal = {
+  nome: 'Jade',
+  idade: 10,
+  comida: 'petisco',
+};
+console.log(Object.keys(animal));
+console.log(Object.values(animal));
+console.log(Object.entries(animal));
