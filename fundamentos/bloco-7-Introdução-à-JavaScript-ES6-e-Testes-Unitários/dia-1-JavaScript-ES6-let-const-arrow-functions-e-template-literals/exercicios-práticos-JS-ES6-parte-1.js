d7647fa94c74833c43de214f650f32833047348c
Fabio Henrique Luiz <fabiohenrique@hotmail.com>
2022-08-18 11:11:21 -0300

    Att exercicios de conteudo

--- fundamentos/bloco-7-Introdução-à-JavaScript-ES6-e-Testes-Unitários/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/exercicios-práticos-JS-ES6-parte-1.js
+++ fundamentos/bloco-7-Introdução-à-JavaScript-ES6-e-Testes-Unitários/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/exercicios-práticos-JS-ES6-parte-1.js
@@ -44,23 +44,23 @@ console.log(sortOddsAndEvens(array));
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-  const sortOddsAndEvens = () => {
-    oddsAndEvens[0] = 2;
-    oddsAndEvens[1] = 3;
-    oddsAndEvens[2] = 4;
-    oddsAndEvens[3] = 7; 
-    oddsAndEvens[4] = 10;
-    oddsAndEvens[5] = 13;
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
 
-    return oddsAndEvens;
-  };
+  return oddsAndEvens;
+};
 
-  const sortedArray = sortOddsAndEvens();
-  console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`);
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`);
 
-  // Bônus array.sort()
+// Bônus array.sort()
 
-  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const sortArrayBonus = (array) => {
   const sortOddsAndEvens = array.sort((a, b) => a - b);
