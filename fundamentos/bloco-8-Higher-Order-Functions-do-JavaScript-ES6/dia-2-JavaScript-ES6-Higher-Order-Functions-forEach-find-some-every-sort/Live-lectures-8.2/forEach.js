ca78a68434eb098c471f179c9a2e9b76a3b80916
Fabio Henrique Luiz <fabiohenrique@hotmail.com>
2022-08-28 15:54:44 -0300

    Aula 8.2

--- fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/dia-2-JavaScript-ES6-Higher-Order-Functions-forEach-find-some-every-sort/Live-lectures-8.2/forEach.js
+++ fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/dia-2-JavaScript-ES6-Higher-Order-Functions-forEach-find-some-every-sort/Live-lectures-8.2/forEach.js
@@ -2,13 +2,44 @@ const numbers = [2, 6, 9, 15, 3, 20, 41, 305];
 
 // Crie um novo array que contenha cada elemento de 'numbers' multiplicado por 3.
 
+
+// O forEach abaixo resgata cada (elemento) do meu array e multiplica por 3
+// Então (forEach-para cada) elemnto de (numbers-array) foi multiplicado por 3
+
+numbers.forEach((elemento) => {
+  console.log(elemento * 3);
+})
+
+// Trazendo só o indice como retorno
+
+numbers.forEach((elemento, indice) => {
+  console.log(indice);
+})
+
+// Trazendo só o array como retorno
+
+numbers.forEach((elemento, indice, array) => {
+  console.log(array);
+})
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+const numbers = [2, 6, 9, 15, 3, 20, 41, 305];
+
+// Crie um novo array que contenha cada elemento de 'numbers' multiplicado por 3.
+
 const newArray = [];
 
+
+numbers.forEach((valor) => {
+  newArray.push(valor * 3);
+})
 // FUNÇÃO forEach: executa uma lógica a cada elemento de um array.
 
-numbers.forEach((_valor, _indice, array) => {
-  newArray.push(array);
-})
 
 console.log(newArray);
 
+// Cada uma das opções dentro do forEach abaixo representa algo de um array, sendo assim o primiero paramentro (valor) representa o valor dentro do array sendo de uso obrigatório a primeira posição o nome não importa pode ser qualquer um ele sempre será o valor do array, o segundo paramentro representado por (indice) significa a posição dentro do array começando a partir do 0 o terceiro valor chamado (array) representa o array todo sendo seu uso opcional também.
+
+numbers.forEach((elemento, indice, array) => { 
+})
--- fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/dia-2-JavaScript-ES6-Higher-Order-Functions-forEach-find-some-every-sort/Live-lectures-8.2/some-find.every.js
+++ fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/dia-2-JavaScript-ES6-Higher-Order-Functions-forEach-find-some-every-sort/Live-lectures-8.2/some-find.every.js
@@ -10,11 +10,30 @@ const users = [
 
 // Pelo menos uma das pessoas dirige?
 
+// A const isDriver = (users - nome do array acima) (.some) que é a HOFs com o parametro (elemento) usado de parametro na callback =  element.isDriver === true
+
 const isDriver = users.some((element) => element.isDriver === true);
 
 console.log(isDriver);
 
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 // FUNÇÃO find: verifica cada elemento do array e retorna o primeiro que passa no critério.
+
+
+const users = [
+  { firstName: 'Homer', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Marge', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Bart', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Lisa', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Maggie', lastName: 'Simpson', isDriver: true }
+];
+
+// A const quemDirige = (users - nome do array acima) (.find) que é a HOFs com o parametro (elemento) usado de parametro na callback =  element.isDriver === true irá retorna o primeiro que passa no critério ou seja irá retorna (Homer) que foi a primeira que se enquadrou nos parametros.
 
 const quemDirige = users.find((element) => element.isDriver === true);
 
@@ -24,12 +43,23 @@ console.log(`A primeira pessoa que dirige da família Simpson é ${quemDirige.fi
 
 // FUNÇÃO every: verifica cada elemento do array e retorna um true ou false, caso todos os elementos passem na condição.
 
+const users = [
+  { firstName: 'Homer', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Marge', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Bart', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Lisa', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Maggie', lastName: 'Simpson', isDriver: true }
+];
+
+// A const todoMundoDirige = (users - nome do array acima) (.every) que é a HOFs com o parametro (elemento) usado de parametro na callback =  element.isDriver === true irá retorna se todos dirigem como isso não é real irá retorna false
+
 const todoMundoDirige = users.every((element) => element.isDriver === true);
 
 console.log(todoMundoDirige);
 
-// operador ternário
+// operador ternário = (?) usamos da maneira abaixo
 // condição true ou false
-const resposta = todoMundoDirige ? 'Sim!' : 'Não!';
+
+let resposta = todoMundoDirige ? 'Sim!' : 'Não!';
 
 console.log(`Todo mundo dirige? ${resposta}`);
