c0f5b0884d492d0d77ce2fc3cb42e3547b90acdb
fabio-luiz <fabiohenrique_2@hotmail.com>
2023-03-03 08:52:45 -0300

    exercicios/back-end-modulo-3-6.3

---
+++ Modulo-3-Back-End/Bloco-6-Node-js-ORM-e-Autenticacao/Dia-3-ORM-Associations-N-N-e-Transactions/course-conteudo/src/services/employee.service.js
@@ -0,0 +1,9 @@
+// src/services/employee.service.js
+// ... 
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+
+const env = process.env.NODE_ENV || 'development';
+// Ajustamos para usar a configuração correta para nosso ambiente
+const sequelize = new Sequelize(config[env]);
+// ...
